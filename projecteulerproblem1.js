00df4f7073b1e7c0976162f114525774b710e0fe
brandonhenning <34727711+brandonhenning@users.noreply.github.com>
2018-01-06 15:01:53 -0700

    Update projecteulerproblem1.js

--- projecteulerproblem1.js
+++ projecteulerproblem1.js
@@ -1,9 +1,15 @@
 /*Project Euler Problem: https://projecteuler.net/problem=1
 
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 
-I thought about solving this problem with a Javascript function using a ‘divisible by’ test. I wrote the function to take in a number and then starting at zero have it count up to the number checking at every integer if it is divisible by either three or five. If the integer passes that test it is added to the total. The function then returns the total of all the numbers between zero and the input number.
+I thought about solving this problem with a Javascript function using a 
+‘divisible by’ test. I wrote the function to take in a number and then starting at zero have it 
+count up to the number checking at every integer if it is divisible by either three or five. If 
+the integer passes that test it is added to the total. The function then returns the total 
+of all the numbers between zero and the input number.
+
 Below is the function.
 */
 
